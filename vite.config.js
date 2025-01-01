25bab3be03d068fc2de060892af380200a29802f
leesangyoun <lsylsy0615@naver.com>
2025-01-02 05:04:32 +0900

    four

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // GitHub Pages 또는 다른 경로에 맞게 설정
   plugins: [react()],
 });
